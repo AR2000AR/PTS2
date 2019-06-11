5dea02a61ef23e7a992a6769e80486593289435a
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-06-11 02:42:19 +0200

    Gestion des profils
    Non tésté

---
+++ src/gestionDeDonnee/NoProfileException.java
@@ -0,0 +1,12 @@
+package gestionDeDonnee;
+
+public class NoProfileException extends Exception {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoProfileException(String msg) {
+		super(msg);
+	}
+}
