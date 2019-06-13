3274bac322716cc7d93531261950c60e63a6f7bb
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-06-13 16:45:57 +0200

    Javadoc + ajustements
    Modification de la taille des tableau boolean[][][]

--- src/gestionDeDonnee/GestionDeDonnee.java
+++ src/gestionDeDonnee/GestionDeDonnee.java
@@ -39,6 +39,15 @@ public class GestionDeDonnee {
 	private static InputStream profils_file = null;
 
 	// -------------------------------------------
+	/**
+	 * Crée un objet <b>GestionDeDonnee</b>. Si c'est le premier les attributs
+	 * statics sont initialisés
+	 *
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 * @throws JDOMException
+	 */
 	public GestionDeDonnee() throws SAXException, IOException, ParserConfigurationException, JDOMException {
 		if (level_file == null) {
 			level_file = getClass().getClassLoader().getResourceAsStream("niveau.xml");
@@ -68,6 +77,11 @@ public class GestionDeDonnee {
 		}
 	}
 
+	/**
+	 * Lit un score depuis un <b>Element</b> et l'ajoute à la liste de scores
+	 *
+	 * @param scoreElement - Un <b>Element</b> qui contien les info sur le score
+	 */
 	private void addScore(Element scoreElement) {
 		int time = Integer.parseInt(scoreElement.getChildTextTrim("time"));
 		String name = scoreElement.getChildTextTrim("name");
@@ -75,6 +89,14 @@ public class GestionDeDonnee {
 		Collections.sort(scores);
 	}
 
+	/**
+	 * Ajoute un score à la liste de score pour le niveau indiqué en paramètre.
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param niveau     - numéro du niveau. [0-5]
+	 * @param score
+	 */
 	private void addScore(int context, int difficulte, int niveau, Score score) {
 		Element newScore = new Element("score");
 		Element name = new Element("name");
@@ -87,6 +109,14 @@ public class GestionDeDonnee {
 				.addContent(newScore);
 	}
 
+	/**
+	 * Revoie un <b>FileWriter</b> pour le fichier dont le nom est passé en
+	 * paramètre
+	 *
+	 * @param fileName - Nom du ficher
+	 * @return <b>FileWriter</b>
+	 * @throws IOException
+	 */
 	private FileWriter getFileWriterFromName(String fileName) throws IOException {
 		return new FileWriter(getClass().getClassLoader().getResource(fileName).getPath());
 	}
@@ -95,14 +125,14 @@ public class GestionDeDonnee {
 	 * Renvoie une chaine de caractère décrivant un niveau
 	 *
 	 * @author Audrézet Rémi
-	 * @param mode       - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
 	 * @param difficulte - difficulte du niveau [0-3]
 	 * @param niveau     - numéro du niveau. [0-5]
-	 * @return
+	 * @return - Chaine formaté
 	 * @throws NiveauInvalide
 	 * @throws NiveauNonTrouve
 	 */
-	public String getLevel(int mode, int difficulte, int niveau) throws NiveauInvalide, NiveauNonTrouve {
+	public String getLevel(int context, int difficulte, int niveau) throws NiveauInvalide, NiveauNonTrouve {
 		if ((difficulte < 0) || (difficulte > 3) || (niveau < 0) || (niveau > 5))
 			throw new NiveauInvalide("Le niveau " + niveau + " n'est pas un niveau valide");
 		NodeList domNiveaux = xmlNiveaux.getElementsByTagName("niveau");
@@ -110,7 +140,7 @@ public class GestionDeDonnee {
 			Node niveauI = domNiveaux.item(i);
 			if (niveauI.getNodeType() == Node.ELEMENT_NODE) {
 				NamedNodeMap attrs = niveauI.getAttributes();
-				if ((attrs.getNamedItem("nbContext").getNodeValue().equals(Integer.toString(mode)))
+				if ((attrs.getNamedItem("nbContext").getNodeValue().equals(Integer.toString(context)))
 						&& (attrs.getNamedItem("nbDifficulte").getNodeValue().equals(Integer.toString(difficulte)))
 						&& (attrs.getNamedItem("nbNiveau").getNodeValue().equals(Integer.toString(niveau)))) {
 					NodeList niveauChildNode = niveauI.getChildNodes();
@@ -124,6 +154,16 @@ public class GestionDeDonnee {
 		throw new NiveauNonTrouve("Le niveau indiqué n'a pas été trouvé. Le fichier peut être endomagé");
 	}
 
+	/**
+	 * Trouve un <b>Element</b> pour le niveau demandé. Si il n'existe pas il est
+	 * crée.
+	 *
+	 * @param listNiveaux - <b>List</b> d'<b>Element</b>
+	 * @param context     - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte  - difficulte du niveau [0-3]
+	 * @param niveau      - numéro du niveau. [0-5]
+	 * @return
+	 */
 	private Element getNiveauElement(List<Element> listNiveaux, int context, int difficulte, int niveau) {
 		for (Element element : listNiveaux) {
 			List<Attribute> attributes = element.getAttributes();
@@ -158,6 +198,13 @@ public class GestionDeDonnee {
 		return nouvNiveau;
 	}
 
+	/**
+	 * Renvoie l'<b>Element</b> pour le profil dont le nom est donnée en paramètre
+	 *
+	 * @param profilName - Nom du profil
+	 * @return - L'<b>Element</b> pour le profil
+	 * @throws NoProfileException Si le profil n'existe pas une erreur est soulevé
+	 */
 	public Element getProfil(String profilName) throws NoProfileException {
 		List<Element> profilList = xmlProfiles.getRootElement().getChildren();
 		if (getProfileNameList().indexOf(profilName) == -1)
@@ -169,6 +216,11 @@ public class GestionDeDonnee {
 		return null; // Ne devrais jamais être attein.
 	}
 
+	/**
+	 * Revoie la liste des noms de profils
+	 *
+	 * @return - <b>List</b> de <b>String</b>
+	 */
 	public List<String> getProfileNameList() {
 		List<Element> profilElements = xmlProfiles.getRootElement().getChildren();
 		List<String> result = new ArrayList<String>();
@@ -178,9 +230,16 @@ public class GestionDeDonnee {
 		return result;
 	}
 
+	/**
+	 * Revoie un tableau à trois dimentions contenant le détaille des niveaux fini
+	 *
+	 * @param profilName - Nom du profil où chercher la progression
+	 * @return <b>Boolean</b>[2][4][6]
+	 * @throws NoProfileException
+	 */
 	public Boolean[][][] getProgression(String profilName) throws NoProfileException {
 		Element profil = getProfil(profilName);
-		Boolean[][][] result = new Boolean[2][4][7];
+		Boolean[][][] result = new Boolean[2][4][6];
 		for (int i = 0; i < result.length; i++) {
 			for (int j = 0; j < result[i].length; j++) {
 				for (int k = 0; k < result[i][j].length; k++) {
@@ -200,6 +259,13 @@ public class GestionDeDonnee {
 		return result;
 	}
 
+	/**
+	 * Revoie le nombre de niveaux finis
+	 *
+	 * @param profilName - Nom du profil où chercher la progression
+	 * @return <b>int</b>
+	 * @throws NoProfileException
+	 */
 	public int getProgressionValue(String profilName) throws NoProfileException {
 		Boolean[][][] progTable = getProgression(profilName);
 		int prog = 0;
@@ -215,15 +281,37 @@ public class GestionDeDonnee {
 		return prog;
 	}
 
+	/**
+	 * Revoi une <b>List</b> de <b>Score</b> pour la difficulté (niveau = 6)
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @return - <b>List</b> de <b>Score</b>
+	 */
 	public List<Score> getScore(int context, int difficulte) {
 		return getScore(context, difficulte, 6);
 	}
 
+	/**
+	 * Renvoi une <b>List</b> de <b>Score</b> pour le niveau indiqué en paramètre.
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param niveau     - numéro du niveau. [0-5]
+	 * @return - <b>List</b> de <b>Score</b>
+	 */
 	public List<Score> getScore(int context, int difficulte, int niveau) {
 		loadScores(context, difficulte, niveau);
 		return scores;
 	}
 
+	/**
+	 * Charge la liste de <b>Score</b> pour le niveau passé en argument
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param niveau     - numéro du niveau. [0-5]
+	 */
 	private void loadScores(int context, int difficulte, int niveau) {
 		scores.clear();
 		List<Element> scoreElementList = getNiveauElement(xmlScores.getRootElement().getChildren("niveau"), context,
@@ -233,6 +321,13 @@ public class GestionDeDonnee {
 		}
 	}
 
+	/**
+	 * Crée un nouveau profil avec le nom fourni en paramètre
+	 *
+	 * @param nom - Nom du profil
+	 * @return <b>False</b> si le profil n'a pas été crée sinon <b>True</b>
+	 * @throws IOException
+	 */
 	public boolean newProfil(String nom) throws IOException {
 		List<String> nomsExistant = getProfileNameList();
 		if (nomsExistant.indexOf(nom) == -1) {
@@ -249,36 +344,71 @@ public class GestionDeDonnee {
 	 * Sauvegarde un score pour un niveau
 	 *
 	 * @author Audrézet Rémi
-	 * @param mode      - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param context   - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
 	 * @param difficlte - Difficulte du niveau [0-3]
 	 * @param niveau    - Numéro du niveau. [0-5]
 	 * @param score     - Le score à sauvegarder.
 	 * @param pseudo    - Le pseudo du joueur.
 	 * @throws IOException
 	 */
-	public void saveScore(int mode, int difficulte, int niveau, int score, String pseudo) throws IOException {
-		saveScore(mode, difficulte, niveau, new Score(pseudo, score));
+	public void saveScore(int context, int difficulte, int niveau, int score, String pseudo) throws IOException {
+		saveScore(context, difficulte, niveau, new Score(pseudo, score));
 	}
 
-	public void saveScore(int mode, int difficulte, int niveau, Score score) throws IOException {
-		loadScores(mode, difficulte, niveau);
-		addScore(mode, difficulte, niveau, score);
+	/**
+	 * Ajoute un score et pour le niveau indiqué en paramètre et sauvegarde dans le
+	 * fichier.
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param niveau     - numéro du niveau. [0-5]
+	 * @param score      - <b>Score</b> à ajouter
+	 * @throws IOException
+	 */
+	public void saveScore(int context, int difficulte, int niveau, Score score) throws IOException {
+		loadScores(context, difficulte, niveau);
+		addScore(context, difficulte, niveau, score);
 		saveXML(xmlScores, getFileWriterFromName("score.xml"));
 	}
 
-	public void saveScore(int mode, int difficulte, int score, String pseudo) throws IOException {
-		saveScore(mode, difficulte, 6, score, pseudo);
+	/**
+	 * Ajoute un score et pour la difficulté indiqué en paramètre et sauvegarde dans
+	 * le fichier.
+	 *
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param score      - <b>Score</b> à ajouter
+	 * @throws IOException
+	 */
+	public void saveScore(int context, int difficulte, int score, String pseudo) throws IOException {
+		saveScore(context, difficulte, 6, score, pseudo);
 	}
 
+	/**
+	 * Ecrie le <b>Document</b> dans le fichier forunit par le <b>FileWriter</b>
+	 *
+	 * @param xmlDocument - Document à sauvegarder
+	 * @param file        - <b>FileWriter</b> à utiliser pour écrire dans le fichier
+	 * @throws IOException
+	 */
 	private void saveXML(org.jdom2.Document xmlDocument, FileWriter file) throws IOException {
 		XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
 		out.output(xmlDocument, file);
 		// out.output(xmlDocument, System.out);
 	}
 
+	/**
+	 * Sauvegarde la progression pour tous les niveaux du tableau
+	 *
+	 * @param nomProfil - Nom du profil pour la sauvegarde
+	 * @param fini      - Tableau de <b>boolean</b>[2][4][6]
+	 * @throws NoProfileException       - Le profil indiqué n'existe pas
+	 * @throws IOException
+	 * @throws TableauMalformeException - Le tableau n'a pas les bonnes dimmentions
+	 */
 	public void setProgression(String nomProfil, boolean[][][] fini)
 			throws NoProfileException, IOException, TableauMalformeException {
-		if ((fini.length == 2) && (fini[0].length == 4) && ((fini[0][0].length == 5) || (fini[0][0].length == 6))) {
+		if ((fini.length == 2) && (fini[0].length == 4) && ((fini[0][0].length == 4) || (fini[0][0].length == 6))) {
 			for (int i = 0; i < fini.length; i++) {
 				for (int j = 0; j < fini[i].length; j++) {
 					for (int k = 0; k < fini[i][j].length; k++) {
@@ -290,6 +420,17 @@ public class GestionDeDonnee {
 			throw new TableauMalformeException("le tableau à trois dimention n'a pas la bonne dimmention");
 	}
 
+	/**
+	 * Sauvegarde la progression pour le niveau passé en paramètre
+	 *
+	 * @param nomProfil  - Nom du profil pour la sauvegarde
+	 * @param context    - <b>false</b> diurne ou <b>true</b> nocturne. [0-1]
+	 * @param difficulte - difficulte du niveau [0-3]
+	 * @param score      - <b>Score</b> à ajouter
+	 * @param fini
+	 * @throws NoProfileException - Le profil indiqué n'existe pas
+	 * @throws IOException
+	 */
 	public void setProgression(String nomProfil, int context, int difficulte, int niveau, boolean fini)
 			throws NoProfileException, IOException {
 		Element profil = getProfil(nomProfil);
