a77237db44ba23d47883e559138615304f2c966a
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-06-17 18:06:13 +0200

    Corrections img

--- src/controller/CompetitionController.java
+++ src/controller/CompetitionController.java
@@ -19,222 +19,36 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 public class CompetitionController extends PlateauController {
 
+	static Timer monTimer;
+
+	static Image imBtnPass = null;
 	@FXML
 	private ImageView btnPasseNv;
-
 	@FXML
 	private Label lblTime;
-	static Timer monTimer;
-	Image imBtnPass = new Image("file:src/image/imageBoutonPasserNiveau.png");
 	protected String[] codeGeneration = new String[6];
 	protected String[] codeSolution = new String[6];
 
 	protected boolean[] nvFait = { false, false, false, false, false, false };
 	private int n = 0;
 
+	protected int temoinTour = 0;
+
+	protected int temoinTour2 = 0;
+
 	@Override
-	public void initialize() {
-		try {
-			Platform.runLater(() -> {
-				gc1 = canvas1.getGraphicsContext2D();
-
-				GestionDeDonnee g;
-				try {
-					g = new GestionDeDonnee();
-					for (int i = 0; i < 6; i++) {
-						codeGeneration[i] = g.getLevel(modeDiurne, diff, i);
-						codeSolution[i] = g.getLevelSoluce(modeDiurne, diff, i);
-					}
-				} catch (SAXException | IOException | ParserConfigurationException | JDOMException | NiveauInvalide | NiveauNonTrouve e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					Platform.exit();
-				}
-
-				System.out.println("Mode Diurne : " + modeDiurne + "\n Difficulte = " + diff);
-
-				
-
-				try {
-					initialiserPlateau(gc1, n);
-				} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				initPiece();
-			});
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		monTimer = new Timer(this);
-		Thread t = new Thread(monTimer);
-		t.start();
-
-		btnPasseNv.setImage(imBtnPass);
-		btnSolution.setImage(imBtnSolution);
-		btnRetourMenuSelection.setImage(imBtnRetour);
-		btnRenitialiser.setImage(ReniImg);
-
+	void clicRetourMenu() throws IOException {
+		getMainClass().choixComp(true);
+		monTimer.stop();
 	}
 
 	public void ecrireTemps(double d) {
 		Platform.runLater(() -> {
 			lblTime.setText(Double.toString(d));
 		});
-	}
-
-	@Override
-	public void testJeuFini() throws JDOMException, SAXException, ParserConfigurationException, IOException {
-		int k = 0;
-		for (int i = 0; i < 3; i++) {
-			if (estPlacer[i]) {
-				k++;
-			}
-			}
-			if (k == 3) {
-				estPlacer[0] = false;
-				estPlacer[1] = false;
-				estPlacer[2] = false;
-
-				nvFait[n] = true;
-				passerNiveau();
-
-			}
-			if (toutLesNvFait() && !solutionDevoile) {
-				try {
-					GestionDeDonnee g = new GestionDeDonnee();
-					g.saveScore(modeDiurne, diff, (int)Math.round(monTimer.getTime()),getProfilName());
-				} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-				
-				getMainClass().showScores(modeDiurne, diff);;
-				System.out.println("#JEU FINI#");
-				monTimer.stop();
-			}
-		
-	}
-
-	private boolean toutLesNvFait() {
-		boolean test = true;
-		int i = 0;
-		while (i < 6 && test) {
-			test = nvFait[i];
-			i++;
-		}
-		return test;
-	}
-
-	protected int temoinTour = 0;
-	protected int temoinTour2 = 0;
-
-	@FXML
-	void passerNiveau() {
-		
-		try {
-			
-			
-			if (!toutLesNvFait()) {
-				n++;
-				System.out.println("n = " + n);
-				if (n < 6) {
-					majPlateau();
-					renitialiserP();
-				} else {
-					n = 0;
-					majPlateau();
-					renitialiserP();
-				}
-			}
-			
-			
-			
-		} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void majPlateau() {
-		gc1.clearRect(0, 0, 400, 400);
-		System.out.println("MAJ PLATEAU ICI NV METHODE");
-
-		gc1.drawImage(imgPlateau, 0, 0, 400, 400);
-
-		s = "ERREUR CHARGEMENT COORDONNEE";
-
-		s = codeGeneration[n];
-
-		System.out.println(s);
-
-		double x1, y1, x2, y2, x3, y3, x4, y4;
-
-		x1 = 0;
-		y1 = 0;
-		x2 = 0;
-		y2 = 0;
-		x3 = 0;
-		y3 = 0;
-		x4 = 0;
-		y4 = 0;
-
-		x1 = 100 * (Integer.parseInt(String.valueOf(s.charAt(1))));
-		y1 = 100 * (Integer.parseInt(String.valueOf(s.charAt(2))));
-		x2 = 100 * (Integer.parseInt(String.valueOf(s.charAt(4))));
-		y2 = 100 * (Integer.parseInt(String.valueOf(s.charAt(5))));
-		x3 = 100 * (Integer.parseInt(String.valueOf(s.charAt(7))));
-		y3 = 100 * (Integer.parseInt(String.valueOf(s.charAt(8))));
-		x4 = 100 * (Integer.parseInt(String.valueOf(s.charAt(10))));
-		y4 = 100 * (Integer.parseInt(String.valueOf(s.charAt(11))));
-
-		/*
-		 * System.out.println(x1); System.out.println(y1); System.out.println(x2);
-		 * System.out.println(y2); System.out.println(x3); System.out.println(y3);
-		 * System.out.println(x4); System.out.println(y4);
-		 */
-
-		for (int k = 0; k < 4; k++) {
-			for (int j = 0; j < 4; j++) {
-				plateau[k][j] = new Case(EnumCase.LIBRE);
-			}
-			plateau[0][0] = new Case(EnumCase.INEXISTANT);
-			plateau[3][0] = new Case(EnumCase.INEXISTANT);
-			plateau[0][3] = new Case(EnumCase.INEXISTANT);
-
-		}
-
-		if (Integer.parseInt(String.valueOf(s.charAt(0))) == 1) {
-			gc1.drawImage(imgCochon, x1 + 10, y1 + 10, 80, 80);
-			initierMatriceVirtuel(x1, y1, EnumCase.COCHON);
-		}
-		if (Integer.parseInt(String.valueOf(s.charAt(3))) == 1) {
-			gc1.drawImage(imgCochon, x2 + 10, y2 + 10, 80, 80);
-			initierMatriceVirtuel(x2, y2, EnumCase.COCHON);
-		}
-		if (Integer.parseInt(String.valueOf(s.charAt(6))) == 1) {
-			gc1.drawImage(imgCochon, x3 + 10, y3 + 10, 80, 80);
-			initierMatriceVirtuel(x3, y3, EnumCase.COCHON);
-		}
-		if (Integer.parseInt(String.valueOf(s.charAt(9))) == 1) {
-			gc1.drawImage(imgLoup, x4 + 10, y4 + 10, 80, 80);
-			initierMatriceVirtuel(x4, y4, EnumCase.LOUP);
-		}
-
-	}
-
-	@Override
-	void clicRetourMenu() throws IOException {
-		getMainClass().choixComp(true);
-		monTimer.stop();
 	}
 
 	@Override
@@ -305,11 +119,194 @@ public class CompetitionController extends PlateauController {
 
 	}
 
+	@Override
+	public void initialize() {
+		if (imBtnPass == null) {
+			imBtnPass = new Image(getClass().getResourceAsStream("image/imageBoutonPasserNiveau.png"));
+		}
+		try {
+			Platform.runLater(() -> {
+				gc1 = canvas1.getGraphicsContext2D();
+
+				GestionDeDonnee g;
+				try {
+					g = new GestionDeDonnee();
+					for (int i = 0; i < 6; i++) {
+						codeGeneration[i] = g.getLevel(modeDiurne, diff, i);
+						codeSolution[i] = g.getLevelSoluce(modeDiurne, diff, i);
+					}
+				} catch (SAXException | IOException | ParserConfigurationException | JDOMException | NiveauInvalide
+						| NiveauNonTrouve e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					Platform.exit();
+				}
+
+				System.out.println("Mode Diurne : " + modeDiurne + "\n Difficulte = " + diff);
+
+				try {
+					initialiserPlateau(gc1, n);
+				} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				initPiece();
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		monTimer = new Timer(this);
+		Thread t = new Thread(monTimer);
+		t.start();
+
+		btnPasseNv.setImage(imBtnPass);
+		btnSolution.setImage(imBtnSolution);
+		btnRetourMenuSelection.setImage(imBtnRetour);
+		btnRenitialiser.setImage(ReniImg);
+
+	}
+
+	private void majPlateau() {
+		gc1.clearRect(0, 0, 400, 400);
+		System.out.println("MAJ PLATEAU ICI NV METHODE");
+
+		gc1.drawImage(imgPlateau, 0, 0, 400, 400);
+
+		s = "ERREUR CHARGEMENT COORDONNEE";
+
+		s = codeGeneration[n];
+
+		System.out.println(s);
+
+		double x1, y1, x2, y2, x3, y3, x4, y4;
+
+		x1 = 0;
+		y1 = 0;
+		x2 = 0;
+		y2 = 0;
+		x3 = 0;
+		y3 = 0;
+		x4 = 0;
+		y4 = 0;
+
+		x1 = 100 * (Integer.parseInt(String.valueOf(s.charAt(1))));
+		y1 = 100 * (Integer.parseInt(String.valueOf(s.charAt(2))));
+		x2 = 100 * (Integer.parseInt(String.valueOf(s.charAt(4))));
+		y2 = 100 * (Integer.parseInt(String.valueOf(s.charAt(5))));
+		x3 = 100 * (Integer.parseInt(String.valueOf(s.charAt(7))));
+		y3 = 100 * (Integer.parseInt(String.valueOf(s.charAt(8))));
+		x4 = 100 * (Integer.parseInt(String.valueOf(s.charAt(10))));
+		y4 = 100 * (Integer.parseInt(String.valueOf(s.charAt(11))));
+
+		/*
+		 * System.out.println(x1); System.out.println(y1); System.out.println(x2);
+		 * System.out.println(y2); System.out.println(x3); System.out.println(y3);
+		 * System.out.println(x4); System.out.println(y4);
+		 */
+
+		for (int k = 0; k < 4; k++) {
+			for (int j = 0; j < 4; j++) {
+				plateau[k][j] = new Case(EnumCase.LIBRE);
+			}
+			plateau[0][0] = new Case(EnumCase.INEXISTANT);
+			plateau[3][0] = new Case(EnumCase.INEXISTANT);
+			plateau[0][3] = new Case(EnumCase.INEXISTANT);
+
+		}
+
+		if (Integer.parseInt(String.valueOf(s.charAt(0))) == 1) {
+			gc1.drawImage(imgCochon, x1 + 10, y1 + 10, 80, 80);
+			initierMatriceVirtuel(x1, y1, EnumCase.COCHON);
+		}
+		if (Integer.parseInt(String.valueOf(s.charAt(3))) == 1) {
+			gc1.drawImage(imgCochon, x2 + 10, y2 + 10, 80, 80);
+			initierMatriceVirtuel(x2, y2, EnumCase.COCHON);
+		}
+		if (Integer.parseInt(String.valueOf(s.charAt(6))) == 1) {
+			gc1.drawImage(imgCochon, x3 + 10, y3 + 10, 80, 80);
+			initierMatriceVirtuel(x3, y3, EnumCase.COCHON);
+		}
+		if (Integer.parseInt(String.valueOf(s.charAt(9))) == 1) {
+			gc1.drawImage(imgLoup, x4 + 10, y4 + 10, 80, 80);
+			initierMatriceVirtuel(x4, y4, EnumCase.LOUP);
+		}
+
+	}
+
+	@FXML
+	void passerNiveau() {
+
+		try {
+
+			if (!toutLesNvFait()) {
+				n++;
+				System.out.println("n = " + n);
+				if (n < 6) {
+					majPlateau();
+					renitialiserP();
+				} else {
+					n = 0;
+					majPlateau();
+					renitialiserP();
+				}
+			}
+
+		} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public void setParam(int contexteSelect, int diff) {
 		this.modeDiurne = contexteSelect;
 		this.diff = diff;
 
 		System.out.println("DEPUIS SET PARAM    \n Mode Diurne : " + contexteSelect + "\n Difficulte = " + diff);
+	}
+
+	@Override
+	public void testJeuFini() throws JDOMException, SAXException, ParserConfigurationException, IOException {
+		int k = 0;
+		for (int i = 0; i < 3; i++) {
+			if (estPlacer[i]) {
+				k++;
+			}
+		}
+		if (k == 3) {
+			estPlacer[0] = false;
+			estPlacer[1] = false;
+			estPlacer[2] = false;
+
+			nvFait[n] = true;
+			passerNiveau();
+
+		}
+		if (toutLesNvFait() && !solutionDevoile) {
+			try {
+				GestionDeDonnee g = new GestionDeDonnee();
+				g.saveScore(modeDiurne, diff, (int) Math.round(monTimer.getTime()), getProfilName());
+			} catch (SAXException | IOException | ParserConfigurationException | JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			getMainClass().showScores(modeDiurne, diff);
+			System.out.println("#JEU FINI#");
+			monTimer.stop();
+		}
+
+	}
+
+	private boolean toutLesNvFait() {
+		boolean test = true;
+		int i = 0;
+		while ((i < 6) && test) {
+			test = nvFait[i];
+			i++;
+		}
+		return test;
 	}
 
 }
--- src/controller/MenuCompetitionController.java
+++ src/controller/MenuCompetitionController.java
@@ -2,13 +2,8 @@ package controller;
 
 import java.io.IOException;
 
-import com.sun.javafx.geom.transform.GeneralTransform3D;
-
-import application.Main;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.effect.Bloom;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,55 +11,57 @@ import javafx.scene.input.MouseEvent;
 
 public class MenuCompetitionController extends Controller {
 
+	private static boolean imported = false;
+
+	static Image im1ec;
+
+	static Image im2ec;
+
+	static Image im3ec;
+
+	static Image im4ec;
+
+	static Image im5ec;
+
+	static Image im6ec;
+
+	static Image im1f;
+
+	static Image im2f;
+
+	static Image im3f;
+	static Image im4f;
+	static Image im5f;
+	static Image im6f;
+	static Image imBtnDiurne;
+	static Image imBtnNocturne;
+
+	static Image imBtnStarter;
+	static Image imBtnJunior;
+	static Image imBtnExpert;
+	static Image imBtnMaster;
+	static Image imJouer;
+	static Image imVoirScore;
+
+	static Image imRetour;
 	@FXML
 	private ImageView btnValider;
 
 	@FXML
 	private ImageView btnRetour;
-
 	@FXML
 	private ImageView sectionDiurne;
-
 	@FXML
 	private ImageView sectionNocturne;
-
 	@FXML
 	private ImageView sectionJunior;
 
 	@FXML
 	private ImageView sectionMaster;
-
 	@FXML
 	private ImageView sectionExpert;
-
 	@FXML
 	private ImageView sectionStarter;
-
-	static Image im1ec = new Image("file:src/image/1enCours.png");
-	static Image im2ec = new Image("file:src/image/2enCours.png");
-	static Image im3ec = new Image("file:src/image/3enCours.png");
-	static Image im4ec = new Image("file:src/image/4enCours.png");
-	static Image im5ec = new Image("file:src/image/5enCours.png");
-	static Image im6ec = new Image("file:src/image/6enCours.png");
-
-	static Image im1f = new Image("file:src/image/1enFait.png");
-	static Image im2f = new Image("file:src/image/2enFait.png");
-	static Image im3f = new Image("file:src/image/3enFait.png");
-	static Image im4f = new Image("file:src/image/4enFait.png");
-	static Image im5f = new Image("file:src/image/5enFait.png");
-	static Image im6f = new Image("file:src/image/6enFait.png");
-
-	static Image imBtnDiurne = new Image("file:src/image/imageBoutonDiurne.png");
-	static Image imBtnNocturne = new Image("file:src/image/imageBoutonNocturne.png");
-
-	static Image imBtnStarter = new Image("file:src/image/imageBoutonStarter.png");
-	static Image imBtnJunior = new Image("file:src/image/imageBoutonJunior.png");
-	static Image imBtnExpert = new Image("file:src/image/imageBoutonExpert.png");
-	static Image imBtnMaster = new Image("file:src/image/imageBoutonMaster.png");
-
-	static Image imJouer = new Image("file:src/image/imageBoutonJouer.png");
-	static Image imVoirScore = new Image("file:src/image/imageBoutonVoirScore.png");
-	static Image imRetour = new Image("file:src/image/imageBoutonRetour.png");
 
 	private int contexteSelect = -1; // 0->1
 	private int nv = -1; // 0 -> 5
@@ -77,7 +74,67 @@ public class MenuCompetitionController extends Controller {
 
 	protected boolean valeurTemoin; // 0 => lance competition , 1 => lance score
 
+	@FXML
+	void clicSurRetour() {
+		getMainClass().showMenu();
+	}
+
+	@FXML
+	void clicValider() throws IOException {
+
+		if ((contexteSelect != -1) && (diff != -1)) {
+
+			if (valeurTemoin) {
+				System.out.println("Mode Diurne : " + contexteSelect + "\n Difficulte = " + diff);
+				getMainClass().chargerComp(contexteSelect, diff);
+			} else {
+				getMainClass().showScores(contexteSelect, diff);
+			}
+		}
+
+	}
+
+	@FXML
+	void expertClic(MouseEvent event) {
+		selectDiff[0] = false;
+		selectDiff[1] = false;
+		selectDiff[2] = true;
+		selectDiff[3] = false;
+		switchDiff();
+		diff = 2;
+	}
+
 	public void initialize() {
+
+		if (!imported) {
+
+			im1ec = new Image(getClass().getResourceAsStream("/image/1enCours.png"));
+			im2ec = new Image(getClass().getResourceAsStream("image/2enCours.png"));
+			im3ec = new Image(getClass().getResourceAsStream("image/3enCours.png"));
+			im4ec = new Image(getClass().getResourceAsStream("image/4enCours.png"));
+			im5ec = new Image(getClass().getResourceAsStream("image/5enCours.png"));
+			im6ec = new Image(getClass().getResourceAsStream("image/6enCours.png"));
+
+			im1f = new Image(getClass().getResourceAsStream("image/1enFait.png"));
+			im2f = new Image(getClass().getResourceAsStream("image/2enFait.png"));
+			im3f = new Image(getClass().getResourceAsStream("image/3enFait.png"));
+			im4f = new Image(getClass().getResourceAsStream("image/4enFait.png"));
+			im5f = new Image(getClass().getResourceAsStream("image/5enFait.png"));
+			im6f = new Image(getClass().getResourceAsStream("image/6enFait.png"));
+
+			imBtnDiurne = new Image(getClass().getResourceAsStream("image/imageBoutonDiurne.png"));
+			imBtnNocturne = new Image(getClass().getResourceAsStream("image/imageBoutonNocturne.png"));
+
+			imBtnStarter = new Image(getClass().getResourceAsStream("image/imageBoutonStarter.png"));
+			imBtnJunior = new Image(getClass().getResourceAsStream("image/imageBoutonJunior.png"));
+			imBtnExpert = new Image(getClass().getResourceAsStream("image/imageBoutonExpert.png"));
+			imBtnMaster = new Image(getClass().getResourceAsStream("image/imageBoutonMaster.png"));
+
+			imJouer = new Image(getClass().getResourceAsStream("image/imageBoutonJouer.png"));
+			imVoirScore = new Image(getClass().getResourceAsStream("image/imageBoutonVoirScore.png"));
+			imRetour = new Image(getClass().getResourceAsStream("image/imageBoutonRetour.png"));
+			imported = true;
+		}
 
 		sectionDiurne.setImage(imBtnDiurne);
 		sectionNocturne.setImage(imBtnNocturne);
@@ -110,15 +167,37 @@ public class MenuCompetitionController extends Controller {
 	}
 
 	@FXML
-	void switchContexteBtn01() {
-		this.contexteSelect = 0;
-		switchContexte(this.contexteSelect);
+	void juniorClic(MouseEvent event) {
+		selectDiff[0] = false;
+		selectDiff[1] = true;
+		selectDiff[2] = false;
+		selectDiff[3] = false;
+		switchDiff();
+		diff = 1;
 	}
 
 	@FXML
-	void switchContexteBtn02() {
-		this.contexteSelect = 1;
-		switchContexte(this.contexteSelect);
+	void masterClic(MouseEvent event) {
+		selectDiff[0] = false;
+		selectDiff[1] = false;
+		selectDiff[2] = false;
+		selectDiff[3] = true;
+		switchDiff();
+		diff = 3;
+	}
+
+	public void setParam(boolean b) {
+		this.valeurTemoin = b;
+	}
+
+	@FXML
+	void starterClic(MouseEvent event) {
+		selectDiff[0] = true;
+		selectDiff[1] = false;
+		selectDiff[2] = false;
+		selectDiff[3] = false;
+		switchDiff();
+		diff = 0;
 	}
 
 	void switchContexte(int i) {
@@ -138,6 +217,18 @@ public class MenuCompetitionController extends Controller {
 		}
 	}
 
+	@FXML
+	void switchContexteBtn01() {
+		this.contexteSelect = 0;
+		switchContexte(this.contexteSelect);
+	}
+
+	@FXML
+	void switchContexteBtn02() {
+		this.contexteSelect = 1;
+		switchContexte(this.contexteSelect);
+	}
+
 	void switchDiff() {
 
 		Bloom bloom = new Bloom();
@@ -155,11 +246,10 @@ public class MenuCompetitionController extends Controller {
 		for (int i = 0; i < 4; i++) {
 			if (i != r) {
 				tabObj[i].setEffect(debloom);
-				;
+
 			}
 		}
 		tabObj[r].setEffect(bloom);
-		;
 
 	}
 
@@ -180,74 +270,10 @@ public class MenuCompetitionController extends Controller {
 		for (int i = 0; i < 6; i++) {
 			if (i != r) {
 				tabObj2[i].setEffect(debloom);
-				;
+
 			}
 		}
 		tabObj2[r].setEffect(bloom);
 
-	}
-
-	@FXML
-	void starterClic(MouseEvent event) {
-		selectDiff[0] = true;
-		selectDiff[1] = false;
-		selectDiff[2] = false;
-		selectDiff[3] = false;
-		switchDiff();
-		diff = 0;
-	}
-
-	@FXML
-	void juniorClic(MouseEvent event) {
-		selectDiff[0] = false;
-		selectDiff[1] = true;
-		selectDiff[2] = false;
-		selectDiff[3] = false;
-		switchDiff();
-		diff = 1;
-	}
-
-	@FXML
-	void expertClic(MouseEvent event) {
-		selectDiff[0] = false;
-		selectDiff[1] = false;
-		selectDiff[2] = true;
-		selectDiff[3] = false;
-		switchDiff();
-		diff = 2;
-	}
-
-	@FXML
-	void masterClic(MouseEvent event) {
-		selectDiff[0] = false;
-		selectDiff[1] = false;
-		selectDiff[2] = false;
-		selectDiff[3] = true;
-		switchDiff();
-		diff = 3;
-	}
-
-	@FXML
-	void clicValider() throws IOException {
-
-		if (contexteSelect != -1 && diff != -1) {
-
-			if (valeurTemoin) {
-				System.out.println("Mode Diurne : " + contexteSelect + "\n Difficulte = " + diff);
-				getMainClass().chargerComp(contexteSelect, diff);
-			} else {
-				getMainClass().showScores(contexteSelect, diff);
-			}
-		}
-
-	}
-
-	public void setParam(boolean b) {
-		this.valeurTemoin = b;
-	}
-
-	@FXML
-	void clicSurRetour() {
-		getMainClass().showMenu();
 	}
 }
